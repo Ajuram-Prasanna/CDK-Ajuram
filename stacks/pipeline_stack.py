0e38dcb0263fbed08bf90469193871bd877f1656
Ajuram-Prasanna <198904342+Ajuram-Prasanna@users.noreply.github.com>
2025-02-22 10:11:41 +0530

    modified secret access

--- stacks/pipeline_stack.py
+++ stacks/pipeline_stack.py
@@ -1,41 +1,39 @@
 from aws_cdk import (
-    aws_codepipeline as codepipeline,
     SecretValue,
-    aws_iam as iam,
-    aws_lambda as _lambda,
     Stack,
-    Stage
 )
 from constructs import Construct
-from aws_cdk.pipelines import ShellStep, CodePipelineSource, CodePipeline
+from aws_cdk.pipelines import CodePipelineSource
 import boto3
 
-secret_client = boto3.client('secretsmanager')
-
-response = secret_client.get_secret_value(
-    SecretId='arn:aws:secretsmanager:ap-southeast-1:682853212408:secret:cdk-token'
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    pipelines as pipelines,
 )
-
-class CdkSampleStack(Stack):
-    def __init__(self, scope: Construct, id: str, **kwargs):
+class WorkshopPipelineStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        
+        self.secret_client = boto3.client('secretsmanager')
 
-        # Define the pipeline
-        pipeline = CodePipeline(self, "Pipeline",
-            pipeline_name="AjuramPipeline",
-            synth=ShellStep("Synth",
-                input=CodePipelineSource.git_hub(
-                    "Ajuram-Prasanna/CDK-Ajuram", "main",
-                    authentication=SecretValue.unsafe_plain_text(response['SecretString'])
-                ),
-                commands=[
-                    "npm install -g aws-cdk",
-                    "python -m pip install -r requirements.txt",
-                    "python -m pip list",
-                    "npx cdk synth"
-                ]
-            )
+        self.response = self.secret_client.get_secret_value(
+            SecretId='arn:aws:secretsmanager:ap-southeast-1:682853212408:secret:cdk-token'
         )
 
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Pipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=CodePipelineSource.git_hub(
+                    "Ajuram-Prasanna/CDK-Ajuram", "main",
+                    authentication=SecretValue.unsafe_plain_text(self.response['SecretString'])
+                ),
+                commands=[
+                    "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                    "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
+                    "npx cdk synth",
+                ]
+            ),
+        )
