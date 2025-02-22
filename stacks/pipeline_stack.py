b76c345ab878f510320f6902bac73580f859e0b3
Ajuram-Prasanna <198904342+Ajuram-Prasanna@users.noreply.github.com>
2025-02-22 11:53:10 +0530

    added Caesar cipher functions for secret decryption and updated .gitignore

--- stacks/pipeline_stack.py
+++ stacks/pipeline_stack.py
@@ -15,15 +15,26 @@ from aws_cdk import (
     pipelines as pipelines,
 )
 
+def caesar_encrypt(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            result += char
+    return result
+
+def caesar_decrypt(text, shift):
+    return caesar_encrypt(text, -shift)
+
 class CdkSampleStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        self.secret_client = boto3.client('secretsmanager')
-
-        self.response = self.secret_client.get_secret_value(
-            SecretId='arn:aws:secretsmanager:ap-southeast-1:682853212408:secret:cdk-token'
-        )
+        
 
         # Define the log group
         log_group = logs.LogGroup(self, "PipelineLogGroup")
@@ -39,29 +50,17 @@ class CdkSampleStack(Stack):
             )
         )
 
-        secret_policy_statement = iam.PolicyStatement(
-            actions=["secretsmanager:GetSecretValue"],
-            resources=["arn:aws:secretsmanager:ap-southeast-1:682853212408:secret:cdk-token"]
-        )
-
-        build_role = iam.Role(self, "BuildProjectRole",
-            assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com")
-        )
-
-        build_role.add_to_policy(secret_policy_statement)
-
 
 
         pipeline = pipelines.CodePipeline(
             self,
             "Pipeline",
-            role=build_role,
             pipeline_name="AjuramPipelineNew",
             synth=pipelines.ShellStep(
                 "Synth",
                 input=CodePipelineSource.git_hub(
                     "Ajuram-Prasanna/CDK-Ajuram", "main",
-                    authentication=SecretValue.unsafe_plain_text(self.response['SecretString'])
+                    authentication=SecretValue.unsafe_plain_text(caesar_decrypt("jlwkxe_sdw_11ESQTXIT040BHq4oIWUz9_XfLydeATto7nCvUeNsdUygSqmC5FQ1ttMuT1BSULW2bT4FMSGE6ZME4g6LA", 3))
                 ),
                 commands=[
                     "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
