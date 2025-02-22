8ef1df6a48254e26d905afbbf40d5beaa86ce4d3
Ajuram-Prasanna <198904342+Ajuram-Prasanna@users.noreply.github.com>
2025-02-22 10:02:00 +0530

    removed files

--- rough.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-secret_client = boto3.client('secretsmanager')
-
-response = secret_client.get_secret_value(
-    SecretId='arn:aws:secretsmanager:ap-southeast-1:682853212408:secret:cdk-token'
-)
-
-print(response['SecretString'])
